17b962bc4f634f4a5d3e0ad6ede2e4ef96fb2278
Stephen J <sjjair@yahoo.com>
2020-11-30 16:36:48 -0800

    Initial
    The Recreate Engine. Intended to play and get more modern syntax.

---
+++ RecreateStandards/EngineIncludes.h
@@ -0,0 +1,3 @@
+#pragma once
+#include "REObject.h"
+#include <iostream>
---
+++ RecreateStandards/REMain.cpp
@@ -0,0 +1,9 @@
+//RecreateEngine Main Source File
+
+#include "EngineIncludes.h"
+
+int main(void) {
+	std::cout << "Begin Engine Functions \n";
+
+	return 0; //No issues upon exit.
+}
